d68a90ee21e9944fc45c49337aaecca68730b323
jieun <ggeun1031@naver.com>
2024-03-21 16:38:48 +0900

    no message

--- chap05-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/Application.java
+++ chap05-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/Application.java
@@ -1,7 +1,5 @@
 package com.ohgiraffers.section01.xmlmapper;
 
-import org.apache.ibatis.session.SqlSession;
-
 import java.util.Scanner;
 
 public class Application {
@@ -23,7 +21,7 @@ public class Application {
             switch (no){
                 case 1 : elementTestService.selectCacheTest(); break;
                 case 2 : resultMapSubMenu(); break;
-                case 3 : break;
+                case 3 : elementTestService.selectSqlTest(); break;
                 case 4 : break;
                 default: return;
             }
@@ -39,12 +37,16 @@ public class Application {
             System.out.println("=============<resultMap> 서브메뉴 =================");
             System.out.println(" 1. <resultMap> 테스트 ");
             System.out.println(" 2. <constructor> 테스트 ");
+            System.out.println(" 3. <association> 테스트");
+            System.out.println(" 4. <collection> 테스트");
             System.out.print("메뉴 번호를 입력하세요 : ");
             int no = sc.nextInt();
 
             switch (no){
                 case 1 : elementTestService.selectResultMapTest(); break;
                 case 2 : elementTestService.selectResultMapConstructorTest(); break;
+                case 3 : elementTestService.selectResultMapAssociationTest(); break;
+                case 4 : elementTestService.selectResultMapCollectionTest(); break;
                 default: return;
             }
         }while (true);
